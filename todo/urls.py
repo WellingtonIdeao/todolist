1daa534f4884211b14ea51c683df5105e674b41c
Wellington Ideão <wellington.ideao@gmail.com>
2022-12-28 16:00:12 -0300

    Register the viewset with a router class to generation of the urlconf

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from todo.views import TodoItemCRUDViewSet
 
-urlpatterns = [
-    path('todos/', TodoItemCRUDViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('todos/<int:pk>/', TodoItemCRUDViewSet.as_view(
-        {'get': 'retrieve', 'put': 'update','patch': 'partial_update', 'delete': 'destroy'}))
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+router = DefaultRouter()
+router.register(r'todos',TodoItemCRUDViewSet, basename='todo')
+urlpatterns = router.urls
