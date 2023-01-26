674aa8d35bd927b528b2dea2889450a55c2f12a5
Wellington Ideão <wellington.ideao@gmail.com>
2023-01-26 15:07:41 -0300

    Added custom datetime format

--- setup/settings/base.py
+++ setup/settings/base.py
@@ -101,3 +101,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DATETIME_FORMAT": "%a, %d %b %Y %H:%M:%S"
+}
